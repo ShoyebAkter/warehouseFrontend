5ea4573deb4736b3330307dd5ad05d58bd917af0
Shoyeb180 <shoyebmohammad660@gmail.com>
2022-05-09 00:43:13 +0600

    .gitignore changed

--- src/Components/Login/Social/SocialLogin.js
+++ src/Components/Login/Social/SocialLogin.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
 import { useLocation, useNavigate } from 'react-router-dom';
-import auth from '../../../../firebase.init';
+import auth from '../../../firebase.init';
 import google from '../../../image/google/download.png'
 import Loading from '../../Shared/Loading/Loading';
 
